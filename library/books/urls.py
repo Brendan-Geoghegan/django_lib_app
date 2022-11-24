aa670eba73b3aef62fdc977f1005c13eb5381c1a
Brendan-Geoghegan <brendangeoghegan@outlook.com>
2022-11-24 11:47:42 +0000

    user can create new books

--- library/books/urls.py
+++ library/books/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.home, name="books-home"),
     path("about/", views.about, name="books-about"),
     path("list/", views.list, name="books-list"),
+    path("new/", views.create, name="books-create"),
     path("<int:id>/", views.show, name="books-show")
 ]
 
